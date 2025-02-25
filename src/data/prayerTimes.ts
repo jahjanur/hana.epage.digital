d65cdf37f81fbf69593a6ed9c3700c62fcf8aedd
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2025-02-25 16:30:48 +0100

    Final04

--- src/data/prayerTimes.ts
+++ src/data/prayerTimes.ts
@@ -58,7 +58,7 @@ export const ramadanTimes = [
   { "date": "2025-03-26", "weekday": "e Mërkurë", "fajr": "3:49", "dhuhr": "11:49", "asr": "15:15", "maghrib": "18:01", "isha": "19:29", "special": "Nata e Kadrit" },
   { "date": "2025-03-27", "weekday": "e Enjte", "fajr": "3:47", "dhuhr": "11:49", "asr": "15:16", "maghrib": "18:02", "isha": "19:30" },
   { "date": "2025-03-28", "weekday": "e Premte", "fajr": "3:45", "dhuhr": "11:49", "asr": "15:16", "maghrib": "18:03", "isha": "19:31" },
-  { "date": "2025-03-29", "weekday": "e Shtunë", "fajr": "11:30", "dhuhr": "11:48", "asr": "15:17", "maghrib": "11:55", "isha": "19:33" }
+  { "date": "2025-03-29", "weekday": "e Shtunë", "fajr": "3:43", "dhuhr": "11:48", "asr": "15:17", "maghrib": "16:19", "isha": "19:33" }
 ]
 
 
