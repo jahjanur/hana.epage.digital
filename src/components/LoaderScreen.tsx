4523bb964b54286666d2258994f7024f872dfff4
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2025-02-25 19:58:22 +0100

    Fix SVG filling animation in loader

--- src/components/LoaderScreen.tsx
+++ src/components/LoaderScreen.tsx
@@ -5,7 +5,16 @@ import LogoHanaLoader from '../assets/LogoHanaLoader.svg';
 const LoaderScreen: React.FC = () => {
   return (
     <div className="loader-container">
-      <div className="loader"></div>
+      <div className="loader">
+        <img src={LogoHanaLoader} alt="Hana Logo" style={{ 
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          opacity: 0.2
+        }} />
+      </div>
       <div className="powered-by">
         Powered by <span className="epage">EPAGE</span>
       </div>
